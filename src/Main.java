da335d1ab1f09e85a091bfd529103654463ff4a3
K0lmykov <alosturd@gmail.com>
2024-11-13 00:38:57 +0400

    3.2(0.1)

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //3.2.1
+
+        Gun gun1 = new Gun(2);
+        gun1.shoot();
+        gun1.shoot();
+        gun1.shoot();
+
+        //3.2.1
+    }
+}
